17ea9a80d2962fe6d1894acd7e3a66636af8ab2f
Christopher C Strelioff <chris.strelioff@gmail.com>
2015-04-14 17:26:45 -0700

    data.js -- setup mosaic sound, image file info

---
+++ js/data.js
@@ -0,0 +1,26 @@
+/*
+ * data.js
+ *
+ * Specify sound and images for each mosaic
+ *
+ */
+
+var mosaicData = [
+{
+"mosaic": "1", 
+"sound": "sounds/tether.wav",
+"images": [
+           "images/mouseon/tether.jpg",
+           "images/mouseoff/tether.jpg"
+          ]
+},
+{  
+"mosaic": "2", 
+"sound": "sounds/HN_tinfoil_light.wav",
+"images": [
+           "images/mouseon/HN_tinfoil_light.jpg",
+           "images/mouseoff/HN_tinfoil_light.jpg"
+          ]
+}
+];
+
